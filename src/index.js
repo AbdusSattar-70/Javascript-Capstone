55e93b11ddb8715054aa861565612bc83fb486aa
torobucci <kevintoro254@gmail.com>
2023-03-21 23:29:57 +0300

    implement add new like and record data in Involvement API

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import displayMeals from './modules/displayMeals.js';
 import fetchMeals from './modules/fetchMeals.js';
 import { ApiUrlSearch, involvmentApiUrlLikes } from './modules/API.js';
 import displayLikes from './modules/displayLikes.js';
-
+import likeItem from './modules/likeItem.js';
 const logo = document.querySelector('#logo');
 logo.src = Logo;
 
@@ -15,4 +15,5 @@ window.onload = async () => {
   const likes = await fetchMeals(involvmentApiUrlLikes);
   displayMeals(data, heart);
   displayLikes(likes);
+  likeItem(likes,involvmentApiUrlLikes)
 };
